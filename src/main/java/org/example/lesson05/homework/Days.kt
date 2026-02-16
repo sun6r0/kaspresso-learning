ff0a9a99cb04db9a58e6ca6bc88fd0c8a38c5c8c
sun6r0 <dshvaiko7@gmail.com>
2026-02-16 16:10:31 +0300

    hw05

---
+++ src/main/java/org/example/lesson05/homework/Days.kt
@@ -0,0 +1,12 @@
+package org.example.lesson05.homework
+
+enum class Days {
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/main/java/org/example/lesson05/homework/ScheduleEntity.kt
@@ -0,0 +1,5 @@
+package org.example.lesson05.homework
+
+import java.time.LocalTime
+
+data class ScheduleEntity(val lesson: String, val startTime: LocalTime, val endTime: LocalTime)
